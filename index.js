c0c161c819f588aac26134f4e8c84d831ac62ced
navneet-97 <162593221+navneet-97@users.noreply.github.com>
2024-05-21 23:17:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const io = require("socket.io")(8000, {
+    cors: {
+      origin: "http://127.0.0.1:5500", 
+      methods: ["GET", "POST"]
+    }
+  });
+  
+  const users = {};
+  
+  io.on("connection", (socket) => {
+      // If any new user joins, let other users connected to the server
+      socket.on("new-user-joined", name => {
+          users[socket.id] = name;
+          socket.broadcast.emit("user-joined", name);
+      });
+  
+      // If someone sends a message, broadcast it to other people
+      socket.on("send", (message) => {
+          socket.broadcast.emit("receive", { message: message, name: users[socket.id] });
+      });
+  
+      // If someone leaves the chat, let others know
+      socket.on("disconnect", () => {
+          socket.broadcast.emit("left", users[socket.id]);
+          delete users[socket.id];
+      });
+  });
+  
